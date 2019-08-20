276f2a70c4c0befb6ceb6f5ca5dcd92cfa29cbfe
Toxiiic <872435708@qq.com>
2019-08-20 17:13:33 +0800

    refactor: 原WidgetComponent接口改为抽象类

---
+++ projects/widget-devkit/src/WidgetBase.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface WidgetOptions {
+    title: string,
+    // minCols?: number, maxCols?: number,
+    // minRows?: number, maxRows?: number,
+
+    // TODO: 数据源相关
+    [name: string]: any
+}
+
+export abstract class WidgetBase implements OnInit {
+
+    @Input()
+    option: WidgetOptions
+    
+    abstract ngOnInit (): void
+    onResized (): void { }
+
+
+}
