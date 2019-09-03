9167c2116e343f42a0cfcaf6f4d5a6665aa16553
Toxiiic <872435708@qq.com>
2019-09-03 15:12:40 +0800

    feat: 天气预报加入http请求，加入代理跨域

--- projects/widget-weather/src/lib/widget-weather.component.ts
+++ projects/widget-weather/src/lib/widget-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Widget, WidgetBase } from '@widget/manifest';
+import { HttpClient } from '@angular/common/http';
 
 @Widget({name:"weather"})
 @Component({
@@ -9,13 +10,25 @@ import { Widget, WidgetBase } from '@widget/manifest';
 })
 export class WidgetWeatherComponent extends WidgetBase {
 
-  constructor() {
+
+
+  constructor(
+    private http: HttpClient
+  ) {
     super()
   }
 
   ngOnInit() {
+    this.renderData()
   }
 
   onResized () {}
 
+  renderData () {
+    this.http.get(`weather-api/wmaps/xml/jinan.xml`, { responseType: 'text' }).subscribe(res => {
+      console.log(res)
+    })
+  
+
+  }
 }
--- projects/widget-weather/src/lib/widget-weather.module.ts
+++ projects/widget-weather/src/lib/widget-weather.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { WidgetWeatherComponent } from './widget-weather.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [WidgetWeatherComponent],
   imports: [
+    HttpClientModule
   ],
   exports: [WidgetWeatherComponent]
 })
