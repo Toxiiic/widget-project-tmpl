98f7423e4acf74c893f57179a97010cb9d13a3c8
Toxiiic <872435708@qq.com>
2019-09-03 17:08:49 +0800

    feat: 天气元部件加入天气接口

--- projects/widget-weather/src/lib/widget-weather.component.ts
+++ projects/widget-weather/src/lib/widget-weather.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Widget, WidgetBase } from '@widget/manifest';
 import { HttpClient } from '@angular/common/http';
 
-@Widget({name:"weather"})
+@Widget({ name: "weather" })
 @Component({
   selector: 'lib-widget-weather',
   templateUrl: './widget-weather.component.html',
@@ -10,7 +10,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WidgetWeatherComponent extends WidgetBase {
 
+  cityName: string = 'jinan'
 
+  temperature = '20'
+  weather = ''
+  windPower = ''
+  humidity = ''
+  feelTemp = ''
+  date = ''
 
   constructor(
     private http: HttpClient
@@ -22,13 +29,38 @@ export class WidgetWeatherComponent extends WidgetBase {
     this.renderData()
   }
 
-  onResized () {}
+  onResized() { }
 
-  renderData () {
-    this.http.get(`weather-api/wmaps/xml/jinan.xml`, { responseType: 'text' }).subscribe(res => {
-      console.log(res)
+  renderData() {
+    this.http.get(`weather-api/wmaps/xml/${this.cityName}.xml`, { responseType: 'text' }).subscribe(res => {
+      let xmlDoc = this.createXmlDoc(res)
+      console.log(xmlDoc.querySelectorAll('city')[0])
+      let cityEle = xmlDoc.querySelectorAll('city')[0]
+      this.temperature = cityEle.getAttribute('temNow')
+      this.weather = cityEle.getAttribute('stateDetailed')
+      this.windPower = cityEle.getAttribute('windPower')
+      this.humidity = cityEle.getAttribute('humidity')
+      this.feelTemp = String(parseInt(this.temperature) + Math.floor(Math.random()*2))
+      this.date = new Date().toDateString()
     })
-  
+  }
 
+  createXmlDoc(xmlText: string) {
+    let xmlDoc
+    // try { //Internet Explorer
+    //   xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+    //   xmlDoc.async = "false";
+    //   xmlDoc.loadXML(xmlText);
+    // } catch (e) {
+      try { //Firefox, Mozilla, Opera, etc.
+        let parser = new DOMParser();
+        return xmlDoc = parser.parseFromString(xmlText, "text/xml");
+      }
+      catch (e) { alert(e.message) }
+    // }
+    // try {
+    //   document.write("xmlDoc 已加载，可以使用。");
+    // }
+    // catch (e) { alert(e.message) }
   }
 }
