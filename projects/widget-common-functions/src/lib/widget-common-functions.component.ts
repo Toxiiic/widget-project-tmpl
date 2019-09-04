b98d849689b04ca646d57b51c582e76642b24507
Toxiiic <872435708@qq.com>
2019-09-04 16:34:25 +0800

    style: 常用功能加入图标，样式优化

--- projects/widget-common-functions/src/lib/widget-common-functions.component.ts
+++ projects/widget-common-functions/src/lib/widget-common-functions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Widget, WidgetBase } from '@widget/manifest';
 
 export interface FunctionItem {
@@ -12,21 +12,25 @@ export interface FunctionItem {
   templateUrl: './widget-common-functions.component.html',
   // styles: []
 })
-export class WidgetCommonFunctionsComponent extends WidgetBase {
+export class WidgetCommonFunctionsComponent extends WidgetBase implements AfterViewInit {
 
   functionList: FunctionItem[] = [
+    { name: '制作订单', id: 'a' },
     { name: '制作销售订单', id: 'a' },
     { name: '制作销售订单', id: 'a' },
     { name: '制作销售订单', id: 'a' },
+    { name: '制作销售单', id: 'a' },
     { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
-    { name: '制作销售订单', id: 'a' },
+    { name: '制作销订单', id: 'a' },
+    { name: '制作销售单', id: 'a' },
     { name: '制作销售订单', id: 'a' },
   ]
+
+  @ViewChild('icon1') icon1
+  @ViewChild('icon2') icon2
+  @ViewChild('icon3') icon3
+
+  icons
 
   constructor() {
     super()
@@ -35,8 +39,14 @@ export class WidgetCommonFunctionsComponent extends WidgetBase {
   ngOnInit() {
   }
 
+  ngAfterViewInit () {
+    this.icons = [this.icon1, this.icon2, this.icon3]
+    console.log(this.icon1)
+  }
+
   handleClickFunction (fn: FunctionItem) {
     console.log(`点击 ${fn.name}`)
+    console.log(this.icons)
   }
 
 }
