bf96daee57762f8c04e780c6843c9065be2e8cd7
Toxiiic <872435708@qq.com>
2019-09-05 15:15:52 +0800

    style: 引入dashboard框架运行时 css

--- projects/widget-common-functions/src/lib/cf-setting/cf-setting.component.ts
+++ projects/widget-common-functions/src/lib/cf-setting/cf-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'lib-cf-setting',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CfSettingComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    public modalRef: BsModalRef
+  ) { }
 
   ngOnInit() {
   }
