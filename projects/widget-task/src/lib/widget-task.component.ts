2b559e6791535ebf91ca787a21533235967fa4f0
hanliukui <hanliukui@inspur.com>
2019-09-19 11:20:24 +0800

    完善task元部件点击列表切换

--- projects/widget-task/src/lib/widget-task.component.ts
+++ projects/widget-task/src/lib/widget-task.component.ts
@@ -8,6 +8,7 @@ export class Task {
   subTime: string;
   orgName: string;
   money: number;
+  style: number;
 }
 
 @Component({
@@ -16,6 +17,7 @@ export class Task {
   styleUrls: ['./app.component.css']
 })
 export class WidgetTaskComponent implements OnInit {
+  style = 1;
 
   title = 'tasks';
   types: string[] = [
@@ -24,32 +26,30 @@ export class WidgetTaskComponent implements OnInit {
     '手机费稽查·9',
     '采购请购单·3',
     '销售发货计划·5' ];
-
   tasks: Task[] = [
     {id: 1, name: '魏小鱼',
      imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1669496880,3827964645&fm=26&gp=0.jpg', 
      orderName: '交通费报销单申请',orderCode: 'JT20190917088', subTime: '2019-09-17',
-     orgName: '平台与技术部-云+产品部', money: 8000 },
+     orgName: '平台与技术部-云+产品部', money: 8000, style: 1  },
      {id: 2, name: '魏小鱼',
      imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1669496880,3827964645&fm=26&gp=0.jpg', 
      orderName: '交通费报销单申请',orderCode: 'JT20190917088', subTime: '2019-09-17',
-     orgName: '平台与技术部-云+产品部', money: 8000 },
+     orgName: '平台与技术部-云+产品部', money: 8000, style: 1 },
      {id: 3, name: '魏小鱼',
      imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1669496880,3827964645&fm=26&gp=0.jpg', 
      orderName: '交通费报销单申请',orderCode: 'JT20190917088', subTime: '2019-09-17',
-     orgName: '平台与技术部-云+产品部', money: 8000 },
+     orgName: '平台与技术部-云+产品部', money: 8000, style: 1 },
      {id: 4, name: '魏小鱼',
      imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1669496880,3827964645&fm=26&gp=0.jpg', 
      orderName: '交通费报销单申请',orderCode: 'JT20190917088', subTime: '2019-09-17',
-     orgName: '平台与技术部-云+产品部', money: 8000 },
+     orgName: '平台与技术部-云+产品部', money: 8000, style: 1},
      {id: 5, name: '魏小鱼',
      imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1669496880,3827964645&fm=26&gp=0.jpg', 
      orderName: '交通费报销单申请',orderCode: 'JT20190917088', subTime: '2019-09-17',
-     orgName: '平台与技术部-云+产品部', money: 8000 },
+     orgName: '平台与技术部-云+产品部', money: 8000 , style: 1}
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
 }
