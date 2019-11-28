0927164adb96cd4ffe92f9f834dacfa2ada9d6f4
Toxiiic <872435708@qq.com>
2019-11-28 17:30:42 +0800

    feat: widget core 集成到 widgets project，元部件用 wc 操作

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { WidgetCoreModule } from '@gspwidget/widget-core'
 
 import { AppComponent } from './app.component';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { AModule } from 'projects/a/src/public-api';
+import { AComponent } from 'projects/a/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  entryComponents: [
+    AComponent
+  ],
   imports: [
     BrowserModule,
     ModalModule.forRoot(),
+    WidgetCoreModule,
+    HttpClientModule,
+    AModule
   ],
   providers: [],
   bootstrap: [AppComponent]
