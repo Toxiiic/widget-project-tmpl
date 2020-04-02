0544fe79ff2475e00e63b6fc2221360c2172fe87
Toxiiic <872435708@qq.com>
2020-04-02 15:52:14 +0800

    feat(国际化): 项目框架引入国际化module；package.json加入相关依赖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { WidgetCoreModule } from '@gspwidget/widget-core'
+import { WidgetCoreModule } from '@gspwidget/widget-core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { WidgetLineModule } from 'projects/widget-line/src/public-api';
@@ -20,6 +22,15 @@ import { WidgetLineComponent } from 'projects/widget-line/src/public-api';
     BrowserModule,
     BrowserAnimationsModule,
     ModalModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [HttpClient]
+      }
+    }),
     WidgetCoreModule,
     HttpClientModule,
     WidgetLineModule
